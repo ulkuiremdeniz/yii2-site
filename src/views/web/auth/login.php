ce072f0db0b94b67801a99ce1fa2e5eeae2eeb78
demirmehmet0 <demirmehmet0@yahoo.com>
2023-04-05 18:26:39 +0300

    style: menu style update

--- src/views/web/auth/login.php
+++ src/views/web/auth/login.php
@@ -9,39 +9,46 @@ $this->title = Module::t('Login');
 
 <div class="site-login">
 <div class="row justify-content-center">
-    <div class="col-lg-6">
+    <div class="col-lg-4">
         <div class="card shadow-sm">
             <div class="card-body">
-                <h1 class="h3 mb-3 fw-normal"><?= Html::encode($this->title) ?></h1>
+                <h1 class="h3 mb-3 fw-normal text-center"><?= Html::encode($this->title) ?></h1>
 
-                <?php $form = ActiveForm::begin(['id' => 'login-form',
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
                     'options' => ['class' => 'form-horizontal'],
                     'fieldConfig' => [
                         'horizontalCssClasses' => [
-                            'label' => 'col-sm-3',
-                            'wrapper' => 'col-sm-9',
+                            'label' => 'col-sm-4',
+                            'wrapper' => 'col-sm-8',
                         ],
+                        'template' => "{input}\n{hint}\n{error}",
                         'labelOptions' => ['style' => 'margin-top: 10px;'],
-                    ],    
-                ],
-            ); ?>
+                    ],
+                ]); ?>
                 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'class' => 'form-control form-control-lg']) ?>
-
-                <?= $form->field($model, 'password')->passwordInput(['class' => 'form-control form-control-lg']) ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'class' => 'form-control form-control-lg', 'placeholder' => Module::t('Username')]) ?>
+                <?= '<div class = "clearfix" style = "margin-top:2px;"></div>' .$form->field($model, 'password')->passwordInput(['class' => 'form-control form-control-lg', 'placeholder' => Module::t('Password')]) ?>
                 <?= Html::a(Module::t('Forgot Password!'), ['/site/auth/request-password-reset'], ['style' => 'float: right;']) ?>
-                <?= '<div class = "clearfix"></div>' .
-                $form->field($model, 'rememberMe', ['options' => ['style' => 'margin-top:10px; float:right;']])->checkbox([
+                <div class = "clearfix"></div>
+                <?= 
+                $form->field($model, 'rememberMe', ['options' => ['style' => 'margin-top:0px; float:right;']])->checkbox([
                     'template' => "<div class=\"form-check\">\n{input} {label}\n</div>",
                 ])->label(Module::t('Remember Me'),['style' => 'margin-top: 0px;']) ?>
+                
+                
                 <div class = "clearfix"></div>
-                <div class="d-grid">
-                    <?= '<div class = "clearfix"></div>' .Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <div style="float:right">
+                    <?= '<div class = "clearfix"></div>' .Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button', 'style'=>'float:right; margin-top:0px;']) ?>
                 </div>
+                
+
+                
 
                 <?php ActiveForm::end(); ?>
             </div>
         </div>
     </div>
 </div>
+
 </div>
