fcc737cc4230c2067f9c0c12cdaa68bc16e8f536
demirmehmet0 <demirmehmet0@yahoo.com>
2023-04-15 01:29:13 +0300

    style: add icon

--- src/widgets/Language.php
+++ src/widgets/Language.php
@@ -7,12 +7,15 @@ use yii\base\Widget;
 use yii\helpers\Html;
 use portalium\site\Module;
 use portalium\theme\widgets\Nav;
+use portalium\menu\models\MenuItem;
 
 class Language extends Widget
 {
     public $options;
 
     public $icon;
+    public $display;
+
     public function init()
     {
         if(!$this->icon){
@@ -29,19 +32,45 @@ class Language extends Widget
         foreach ($languages as $key => $value){
             $langItems[] = [
                 'label' => Module::t($value),
-                'url' => ['/site/home/lang','lang' => $key]
+                'url' => ['/site/home/lang','lang' => $key],
             ];
         }
 
         $menuItems[] = [
-            'label' => Module::t($languages[Yii::$app->language]),
+            'label' => $this->generateLabel("Language"),
             'url' => ['/site/home/lang','lang' => Yii::$app->language],
             'items' => $langItems,
+            'display' => $this->display,
         ];
 
         return Nav::widget([
             'options' => $this->options,
             'items' => $menuItems,
         ]);
+    }
+
+    private function generateLabel($text)
+    {
+        $label = "";
+            if(isset($this->display)){
+                switch ($this->display) {
+                    case MenuItem::TYPE_DISPLAY['icon']:
+                        $label = $this->icon;
+                        break;
+                    case MenuItem::TYPE_DISPLAY['icon-text']:
+                        $label = $this->icon . Module::t($text);
+                        break;
+                    case MenuItem::TYPE_DISPLAY['text']:
+                        $label = Module::t($text);
+                        break;
+                    default:
+                        $label = $this->icon . Module::t($text);
+                        break;
+                }
+            }else{
+                $label = $this->icon . Module::t($text);
+            }
+
+        return $label;
     }
 }
--- src/widgets/LoginButton.php
+++ src/widgets/LoginButton.php
@@ -6,10 +6,13 @@ use Yii;
 use yii\base\Widget;
 use yii\helpers\Html;
 use portalium\site\Module;
+use portalium\menu\models\MenuItem;
 
 class LoginButton extends Widget
 {
     public $icon;
+    public $display;
+
     public function init()
     {
         if(!$this->icon){
@@ -21,9 +24,36 @@ class LoginButton extends Widget
     public function run()
     {
         if (Yii::$app->user->isGuest) {
-            return '<li class="nav-item">' . Html::a($this->icon . Module::t('Login'), ['/site/auth/login'], ['class' => 'nav-link']) . '</li>';
+            $this->icon = Html::tag('i', '', ['class' => 'fa fa-sign-in', 'style' => 'min-width: 25px;']);
+            return '<li class="nav-item">' . Html::a($this->generateLabel('Login'), ['/site/auth/login'], ['class' => 'nav-link']) . '</li>';
         } else {
-            return '<li class="nav-item">' . Html::a($this->icon . Module::t('Logout'). ' (' . Yii::$app->user->identity->username . ')',['/site/auth/logout'],['class' => 'nav-link']) . '</li>';
+            $this->icon = Html::tag('i', '', ['class' => 'fa fa-sign-out', 'style' => 'min-width: 25px;']);
+            return '<li class="nav-item">' . Html::a($this->generateLabel('Logout', ' (' . Yii::$app->user->identity->username . ')'),['/site/auth/logout'],['class' => 'nav-link']) . '</li>';
         }
+    }
+
+    private function generateLabel($text, $param = null)
+    {
+        $label = "";
+            if(isset($this->display)){
+                switch ($this->display) {
+                    case MenuItem::TYPE_DISPLAY['icon']:
+                        $label = $this->icon;
+                        break;
+                    case MenuItem::TYPE_DISPLAY['icon-text']:
+                        $label = $this->icon . Module::t($text) . $param;
+                        break;
+                    case MenuItem::TYPE_DISPLAY['text']:
+                        $label = Module::t($text);
+                        break;
+                    default:
+                        $label = $this->icon . Module::t($text) . $param;
+                        break;
+                }
+            }else{
+                $label = $this->icon . Module::t($text) . $param;
+            }
+
+        return $label;
     }
 }
